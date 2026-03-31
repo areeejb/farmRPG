9027a9850e5bb63cddd6cd7a7733c61bc0f457de
Mohammad Jawad Wehbe <mhw07@mail.aub.edu>
2026-03-31 12:14:35 +0300

    added ladders , collisions and farm land

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    public float climbSpeed = 3f;
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    private bool onLadder = false;
 
     void Start()
     {
@@ -20,6 +22,31 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movement * speed;
+        if (onLadder)
+        {
+            rb.gravityScale = 0;
+            rb.linearVelocity = new Vector2(movement.x * speed, movement.y * climbSpeed);
+        }
+        else
+        {
+            rb.gravityScale = 1;
+            rb.linearVelocity = movement * speed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            onLadder = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            onLadder = false;
+        }
     }
 }
